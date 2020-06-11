3831acb8c3cacd81c31ef1dca524cebc523bd055
Collin Jilbert <cjilbert504@gmail.com>
2020-06-10 22:00:30 -0500

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,34 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, func) {
+      let newArr = Object.values(collection);
+      newArr.forEach(e => func(e))
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, func) {
+      let newArr = Object.values(collection);
+      let finalArr = [];
+      newArr.forEach(e => finalArr.push(func(e)));
+      return finalArr;
     },
 
-    reduce: function() {
+    reduce: function(collection, func, acc) {
+      let start = !!acc ? acc : collection[0];
+      let newArr = start === acc ? collection : collection.slice(1);
+      newArr.forEach(e => {
+        start = func(start, e)
+      })
+      return start;
+    },
 
+    find: function(collection, predicate) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
+        collection.forEach(e => {
+        predicate(e);
+        })
     },
 
     functions: function() {
